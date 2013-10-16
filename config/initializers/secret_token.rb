1f390cd7587d464ca9515bac100554d88b8280f3
Jason Forcier <jasonwforcier@gmail.com>
2013-10-15 17:47:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SuiteCloud::Application.config.secret_key_base = 'a8580eb61762ebd3c1df2efb7f91f594e2b467b741604f18e7e57924b9d33a956709e4c30365a2db67e0a61d79d163ec29f86fe3ea3870f5d19886800e787a98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SuiteCloud::Application.config.session_store :cookie_store, key: '_SuiteCloud_session'
