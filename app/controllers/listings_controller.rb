dbdc3ac8c9334f59ab3acc726f9a6c69ff2a1e6d
Jason Forcier <jasonwforcier@gmail.com>
2013-11-21 23:53:33 -0800

    Added pagination to listings index

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,7 +3,7 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @listings = Listing.all
+    @listings = Listing.all.paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
