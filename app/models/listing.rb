db3c838fbaee91e14264d3735f8b96d534ccbfc1
Jason Forcier <jasonwforcier@gmail.com>
2013-11-22 00:15:19 -0800

    Add names to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates :image, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121040217) do
+ActiveRecord::Schema.define(version: 20131122075910) do
 
   create_table "listings", force: true do |t|
     t.string   "city"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20131121040217) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
